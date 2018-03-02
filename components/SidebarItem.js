82ab0624c9dd0b5800ed86a1b2a68f273ea73189
Michael Anderson <michanders@msn.com>
2018-03-02 01:36:29 +0000

    automatically backed up by learn

--- components/SidebarItem.js
+++ components/SidebarItem.js
@@ -10,7 +10,7 @@ export default class SidebarItem extends React.Component {
   render() {
     return (
       <li className="sidebar__item">
-        <a href='#' onClick={onClick} className='sidebar__link'>{this.props.getTitle}</a>
+        <a href='#' onClick={this.props.onClick} className='sidebar__link'>{this.props.getTitle}</a>
       </li>
     );
   }

9e8c26d5ea33d2e1c4bfda4be26d60a400302a53
Michael Anderson <michanders@msn.com>
2018-03-02 01:21:50 +0000

    automatically backed up by learn

--- components/FileView.js
+++ components/FileView.js
@@ -7,7 +7,7 @@ export default class FileView extends React.Component {
   render() {
     return (
       <div className="file-view">
-        <Toolbar onRemove={this.props.onRemove}/>
+        <Toolbar onRemove={this.props.onRemove} onAdd={this.props.onAdd}/>
         <textarea className="file-view__text" value={this.props.file} onChange={this.props.onChange} />
       </div>
     );
